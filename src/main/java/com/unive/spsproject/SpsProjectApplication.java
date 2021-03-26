3aec0f80e5f530ab3521bcbf2a553825821ce046
Filippo Maganza <fmaganza@alpenite.com>
2021-03-26 12:51:38 +0100

    Init project

---
+++ src/main/java/com/unive/spsproject/SpsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.unive.spsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsProjectApplication.class, args);
+	}
+
+}
